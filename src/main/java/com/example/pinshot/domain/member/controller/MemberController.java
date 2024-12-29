993a981f590171be12e77f396ba7abea47f48ab1
tlsrhksgh <sin99145@naver.com>
2024-12-29 20:13:23 +0900

    fix: git controller conflict로 인한 수정

--- src/main/java/com/example/pinshot/domain/member/controller/MemberController.java
+++ src/main/java/com/example/pinshot/domain/member/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.example.pinshot.domain.member.controller;
 
 import com.example.pinshot.domain.member.dto.MemberSignUpRequest;
 import com.example.pinshot.domain.member.service.MemberService;
-import com.example.pinshot.domain.member.service.impl.MemberServiceImpl;
 import com.example.pinshot.global.base.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
